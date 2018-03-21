8f08a916481f8daf76613dc5be2d50abe2e1dff3
engineer135 <everafterk@naver.com>
2018-03-21 11:12:06 +0900

    첫 번째 장고 앱 작성하기, part 3 - view 만들기

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request) :
     return HttpResponse("Hello~~~ You're at the polls index.")
+
+def detail(request, question_id) :
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id) :
+    response = "너는 질문의 결과를 보고 있어 %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id) :
+    return HttpResponse("너는 지금 %s에 투표중이야" % question_id)
