b711b5aedc658d9ed81b17da777ce43d857c7afd
engineer135 <everafterk@naver.com>
2018-03-21 11:23:00 +0900

    첫 번째 장고 앱 작성하기, part 3 - template 만들기

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 # Create your views here.
 def index(request) :
-    return HttpResponse("Hello~~~ You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # 하드코딩 대신 템플릿 사용하자
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list' : latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id) :
     return HttpResponse("You're looking at question %s." % question_id)
