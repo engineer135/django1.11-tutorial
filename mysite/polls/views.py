7b7f6a70cdc220f09df2110ff6ba5772c98421c9
engineer135 <everafterk@naver.com>
2018-03-22 10:25:53 +0900

    첫 번째 장고 앱 작성하기, part 4 - 간단한 폼 만들기

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import Http404, HttpResponse
-from .models import Question
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 
 # Create your views here.
 def index(request) :
@@ -24,8 +25,28 @@ def detail(request, question_id) :
     return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id) :
-    response = "너는 질문의 결과를 보고 있어 %s"
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question':question})
 
 def vote(request, question_id) :
-    return HttpResponse("너는 지금 %s에 투표중이야" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    print(question)
+    try :
+        # 명시적으로 post데이터만 쓰려고 request.POST[] 사용
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist) :
+        print('except!!')
+        # post 자료에 choice 가 없는 경우 keyError 발생
+        # 투표 폼을 다시 보여준다
+        return render(request, 'polls/detail.html', {
+            'question' : question,
+            'error_message' : "초이스한 항목이 없어!",
+        })
+    else :
+        selected_choice.votes += 1
+        selected_choice.save()
+        # 리다이렉트로 리턴. 중복 서브밋 방지
+        # reverse 함수는 url을 하드코딩하지 않도록 도와준다.
+        # 이때 args는 itrable해야하므로, 마지막에 콤마를 꼭 써줘야한다. 안그러면 에러남.
+        # 아래 reverse 함수는 '/polls/3/results/' 같은 문자열을 반환한다.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
